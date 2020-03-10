d1419a1fff489328fa9944f3443ba60c05dfb9b8
Markus Döring <m.doering@mac.com>
2020-03-10 19:01:52 +0100

    Add methods to change parser override configs

--- name-parser/src/main/java/org/gbif/nameparser/ParserConfigs.java
+++ name-parser/src/main/java/org/gbif/nameparser/ParserConfigs.java
@@ -20,4 +20,20 @@ public class ParserConfigs {
     authorship = ParsingJob.preClean(authorship, null);
     return authorships.get(authorship);
   }
+
+  public void setName(String sciname, ParsedName pn) {
+    names.put(ParsingJob.preClean(sciname, null), pn);
+  }
+
+  public ParsedName deleteName(String name) {
+    return names.remove(ParsingJob.preClean(name, null));
+  }
+
+  public void setAuthorship(String authorship, ParsedAuthorship pn) {
+    authorships.put(ParsingJob.preClean(authorship, null), pn);
+  }
+
+  public ParsedAuthorship deleteAuthorship(String authorship) {
+    return authorships.remove(ParsingJob.preClean(authorship, null));
+  }
 }
