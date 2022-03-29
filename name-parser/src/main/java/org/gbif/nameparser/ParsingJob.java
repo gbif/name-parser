002658f28a2bb54742d4a45fdae6db33006c0c8c
Markus Döring <m.doering@mac.com>
2022-03-29 13:23:04 +0200

    Mark undetermined as placeholder name

--- name-parser/src/main/java/org/gbif/nameparser/ParsingJob.java
+++ name-parser/src/main/java/org/gbif/nameparser/ParsingJob.java
@@ -300,7 +300,7 @@ class ParsingJob implements Callable<ParsedName> {
     "(?:[, ]+(" + YEAR_LOOSE + "))?$", Pattern.CASE_INSENSITIVE);
   private static final Pattern PLACEHOLDER_GENUS = Pattern.compile("^(In|Dummy|Missing|Temp|Unknown|Unplaced|Unspecified) (?=[a-z]+)\\b");
   private static final String PLACEHOLDER_NAME = "(?:allocation|awaiting|" +
-    "deleted?|dummy|incertae ?sedis|indetermined|mixed|" +
+    "deleted?|dummy|incertae ?sedis|[iu]ndetermined|mixed|" +
     "not (?:assigned|stated)|" +
     "place ?holder|temp|tobedeleted|" +
     "un(?:accepted|allocated|assigned|certain|classed|classified|cultured|described|det(?:ermined)?|ident|known|named|placed|specified)" +
