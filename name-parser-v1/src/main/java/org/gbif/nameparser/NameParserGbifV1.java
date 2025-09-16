f0ceb7bf56b02a9e3df2fe6983e8a3dfb754cfa7
Markus Döring <mdoering@gbif.org>
2025-09-16 11:23:27 +0200

    Revert "Map new series ranks to gbif vocab"
    This reverts commit 44f36ac3a02d1842f1e0311357f3d62c11d840a3.

--- name-parser-v1/src/main/java/org/gbif/nameparser/NameParserGbifV1.java
+++ name-parser-v1/src/main/java/org/gbif/nameparser/NameParserGbifV1.java
@@ -264,8 +264,6 @@ public class NameParserGbifV1 implements NameParser {
       case SUBDIVISION: return Rank.SUBPHYLUM;
       case INFRADIVISION: return Rank.INFRAPHYLUM;
       case PROLES: return Rank.RACE;
-      case SERIES_BOTANY: return Rank.SERIES;
-      case SUBSERIES_BOTANY: return Rank.SUBSERIES;
       case SECTION_BOTANY: return Rank.SECTION;
       case SUBSECTION_BOTANY: return Rank.SUBSECTION;
 
@@ -290,10 +288,6 @@ public class NameParserGbifV1 implements NameParser {
       case SUPERSECTION_ZOOLOGY:
       case SECTION_ZOOLOGY:
       case SUBSECTION_ZOOLOGY:
-
-      case SUPERSERIES_ZOOLOGY:
-      case SERIES_ZOOLOGY:
-      case SUBSERIES_ZOOLOGY:
 
       case GIGAORDER:
       case MIRORDER:
@@ -337,10 +331,6 @@ public class NameParserGbifV1 implements NameParser {
         return org.gbif.nameparser.api.Rank.SECTION_BOTANY;
       case SUBSECTION:
         return org.gbif.nameparser.api.Rank.SUBSECTION_BOTANY;
-      case SERIES:
-        return org.gbif.nameparser.api.Rank.SERIES_BOTANY;
-      case SUBSERIES:
-        return org.gbif.nameparser.api.Rank.SUBSERIES_BOTANY;
       default:
         return convertEnum(org.gbif.nameparser.api.Rank.class, rank);
     }
